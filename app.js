198c509f3dc1f07c954a5d61ad4282da5abf196e
RagePeanut <dorian.gruselin@hotmail.com>
2018-06-15 15:55:31 +0200

    Added created post checker (edits are ignored)

--- app.js
+++ app.js
@@ -12,21 +12,18 @@ function stream() {
         steemStream.api.setOptions({ url: stream_nodes[0] })
         steemStream.api.streamOperations('irreversible', (error, operation) => {
             if(error) return reject(error);
-            if(operation && operation[0] === 'comment' && operation[1].parent_author === '' && !/@@ -\d+(,\d+)? \+\d+(,\d+)? @@/.test(operation[1].body)) {
-                let metadata;
+            if(operation && operation[0] === 'comment' && operation[1].parent_author === '') {
                 try {
-                    metadata = JSON.parse(operation[1].json_metadata);
-                    // Removing empty strings
-                    let mentions = metadata.users.filter(user => user !== '')
+                    const metadata = JSON.parse(operation[1].json_metadata);
                     // Removing the dots at the end of some mentions (happens with Busy)
-                                                   .map(user => user.replace(/\.+$/, ''))
-                    // Removing website names
-                                                   .filter(user => user.length < 5 || !/\.[a-z]{2,3}$/.test(user))
+                    let mentions = metadata.users.map(user => user.replace(/\.+$/, ''))
+                    // Removing website names, empty names and letter names (for example with 'hola amig@s', '@s' would be added as a mention by some apps)
+                                                 .filter(user => (user.length < 5 && user.length > 1 && user !== '') || !/\.[a-z]{2,3}$/.test(user))
                     // Lower casing the characters
-                                                   .map(user => user.toLowerCase());
+                                                 .map(user => user.toLowerCase());
                     // Removing duplicates
                     mentions = _.uniq(mentions);
-                    processMentions(mentions, operation[1].body, operation[1].author, operation[1].permlink);
+                    processCreatedPost(mentions, operation[1].body, operation[1].author, operation[1].permlink);
                 } catch(err) {}
             }
         });
@@ -35,13 +32,29 @@ function stream() {
         // Putting the node where the error comes from at the end of the array
         stream_nodes.push(stream_nodes.shift());
         stream();
-    })
+    });
+}
+
+function processCreatedPost(mentions, body, author, permlink) {
+    new Promise((resolve, reject) => {
+        steemRequest.api.getContent(author, permlink, (err, res) => {
+            if(err) return reject(err);
+            if(res.last_update === res.created) processMentions(mentions, body, author, permlink);
+        });
+    }).catch(error => {
+        console.error(`Request error (getContent): ${ error.message } with ${ request_nodes[0] }`);
+        // Putting the node where the error comes from at the end of the array
+        request_nodes.push(request_nodes.shift());
+        steemRequest.api.setOptions({ url: request_nodes[0] });
+        console.log(`Retrying with ${ request_nodes[0] }`)
+        processCreatedPost(mentions, body, author, permlink);
+    });
 }
 
 function processMentions(mentions, body, author, permlink) {
     new Promise((resolve, reject) => {
         steemRequest.api.lookupAccountNames(mentions, (err, res) => {
-            if(err) reject(err);
+            if(err) return reject(err);
             const wrongMentions = [];
             // Add each username that got a null result from the API (meaning the user doesn't exist) to the wrongMentions array
             for(let i = 0; i < mentions.length; i++) {
@@ -57,13 +70,13 @@ function processMentions(mentions, body, author, permlink) {
                     });
                     if(!socialNetworkRelated) {
                         sendMessage(wrongMentions);
+                        console.log(wrongMentions, author, permlink);
                     }
-                    console.log(wrongMentions, author, permlink);
                 }
             }
         });
     }).catch(error => {
-        console.error(`Request error: ${ error.message } with ${ request_nodes[0] }`);
+        console.error(`Request error (lookupAccountNames): ${ error.message } with ${ request_nodes[0] }`);
         // Putting the node where the error comes from at the end of the array
         request_nodes.push(request_nodes.shift());
         steemRequest.api.setOptions({ url: request_nodes[0] });
