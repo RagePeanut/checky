59ffc8fd5b1e61883075e4a74a0e90cc865f8a34
RagePeanut <dorian.gruselin@hotmail.com>
2018-06-19 23:09:44 +0200

    Added bot commands

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const _ = require('lodash');
+const fs = require('fs');
 const steemStream = require('steem');
 const steemRequest = require('steem');
 const { posting_key, request_nodes, stream_nodes } = require('./config');
 
 steemRequest.api.setOptions({ url: request_nodes[0] });
+
+let blacklist = [];
+let ignoredMentions = {};
+if(fs.existsSync('data/')) {
+    if(fs.existsSync('data/blacklist.json')) blacklist = JSON.parse(fs.readFileSync('data/blacklist.json', 'utf8'));
+    if(fs.existsSync('data/ignored_mentions.json')) ignoredMentions = JSON.parse(fs.readFileSync('data/ignored_mentions.json', 'utf8'));
+} else fs.mkdirSync('data');
+setInterval(() => {
+    fs.writeFile('data/blacklist.json', JSON.stringify(blacklist), err => {
+        if(err) console.log(err);
+    });
+    fs.writeFile('data/ignored_mentions.json', JSON.stringify(ignoredMentions), err => {
+        if(err) console.log(err);
+    });
+}, 30 * 1000);
 
 stream();
 
@@ -12,15 +28,22 @@ function stream() {
         steemStream.api.setOptions({ url: stream_nodes[0] })
         steemStream.api.streamOperations((error, operation) => {
             if(error) return reject(error);
-            if(operation && operation[0] === 'comment' && operation[1].parent_author === '') {
-                try {
-                    const metadata = JSON.parse(operation[1].json_metadata);
-                    // Removing the punctuation at the end of some mentions and lower casing mentions
-                    let mentions = metadata.users.map(user => user.replace(/[?¿!¡.,;:-]+$/, '').toLowerCase());
-                    // Removing duplicates
-                    mentions = _.uniq(mentions);
-                    processCreatedPost(mentions, operation[1].body, operation[1].author, operation[1].permlink);
-                } catch(err) {}
+            if(operation && operation[0] === 'comment') {
+                if(operation[1].parent_author === '%') {
+                    if(!blacklist.includes(operation[1].author)) {
+                        try {
+                            const metadata = JSON.parse(operation[1].json_metadata);
+                            // Removing the punctuation at the end of some mentions, lower casing mentions and removing duplicates
+                            let mentions = _.uniq(metadata.users.map(user => user.replace(/[?¿!¡.,;:-]+$/, '').toLowerCase()));
+                            // Removing ignored mentions
+                            if(ignoredMentions[operation[1].author]) mentions = mentions.filter(mention => !ignoredMentions[operation[1].author].includes(mention));
+                            if(mentions.length > 0) processCreatedPost(mentions, operation[1].body, operation[1].author, operation[1].permlink);
+                        } catch(err) {}
+                    }
+                } else if(operation[1].parent_author === 'checky') {
+                    const command = /!([A-Za-z]+)(?:\s+(.+))?/.exec(operation[1].body);
+                    if(command[1]) processCommand(command, operation[1].author, operation[1].permlink);
+                }
             }
         });
     }).catch(error => {
@@ -61,7 +84,16 @@ function processMentions(mentions, body, author, permlink, title) {
                 }
             }
             if(wrongMentions.length > 0) {
-                sendMessage(wrongMentions, author, permlink, title);
+                let message = `Hi @${ author },`;
+                if(wrongMentions.length > 1) {
+                    wrongMentions = wrongMentions.map(mention => mention + ',');
+                    wrongMentions[wrongMentions.length-1] = wrongMentions[wrongMentions.length-1].replace(',', '');
+                    wrongMentions[wrongMentions.length-2] = wrongMentions[wrongMentions.length-2].replace(',', ' and');
+                    message = message + ` while checking the users mentioned in this post I noticed that ${ wrongMentions.join(' ') } don't exist on Steem. Maybe you made some typos ?`
+                } else {
+                    message = message + ` the account ${ wrongMentions[0] } mentioned in this post doesn't seem to exist on Steem. Maybe you made a typo ?`;
+                }
+                sendMessage(message, author, permlink, 'Possible wrong mentions found on ' + title);
             }
             console.log(wrongMentions, author);
         });
@@ -75,16 +107,42 @@ function processMentions(mentions, body, author, permlink, title) {
     });
 }
 
-function sendMessage(wrongMentions, author, permlink, title) {
-    let message = `Hi @${ author },`;
-    if(wrongMentions.length > 1) {
-        wrongMentions = wrongMentions.map(mention => mention + ',');
-        wrongMentions[wrongMentions.length-1] = wrongMentions[wrongMentions.length-1].replace(',', '');
-        wrongMentions[wrongMentions.length-2] = wrongMentions[wrongMentions.length-2].replace(',', ' and');
-        message = message + ` while checking the users mentioned in this post I noticed that ${ wrongMentions.join(' ') } don't exist on Steem. Maybe you made some typos ?`
-    } else {
-        message = message + ` the account ${ wrongMentions[0] } mentioned in this post doesn't seem to exist on Steem. Maybe you made a typo ?`;
+function processCommand(command, author, permlink) {
+    switch(command[1]) {
+        case 'start':
+        case 'join':
+            if(blacklist.includes(author)) {
+                _.pull(blacklist, author);
+                sendMessage('Your mentions will from now on be inspected by @checky. If for any reason you want to stop this, reply to any of my posts with "!stop" or "!leave".', author, permlink, `Removed @${ author } from the blacklist`);
+            } else sendMessage('Your mentions were already being inspected by @checky.', author, permlink, 'The blacklist hasn\'t been changed (username not in blacklist)')
+            break;
+        case 'stop':
+        case 'leave':
+            blacklist = _.union(blacklist, [author]);
+            sendMessage('You\'ve been added to @checky\'s blacklist. Your mentions will from now on stop being inspected by @checky. If for any reason you want @checky to start checking your mentions again, reply to any of my posts with "!start" or "!join".', author, permlink, `Added @${ author } to the blacklist`);
+            break;
+        case 'ignore':
+            if(command[2]) {
+                const mentions = command[2].split(/[\s,]+/).filter(mention => mention !== '').map(mention => mention.replace('@', ''));
+                ignoredMentions[author] = _.union(ignoredMentions[author], mentions);
+                sendMessage(`The following mentions will now be ignored when made by you: ${ mentions.join(', ') }.\nIf for any reason you want to make @checky stop ignoring them, reply to any of my posts with "!unignore username1 username2 ...".`, author, permlink, `Added some ignored mentions for @${ author }`);
+            } else sendMessage('You didn\'t specify any username to ignore. Please try again by using the format "!ignore username1 username2 ...".', author, permlink, 'No username specified');
+            break;
+        case 'unignore':
+            if(command[2]) {
+                const mentions = command[2].split(/[\s,]+/).filter(mention => mention !== '').map(mention => mention.replace('@', ''));
+                mentions.forEach(mention => {
+                    if(ignoredMentions[author]) _.pull(ignoredMentions[author], mention);
+                });
+                sendMessage(`The following mentions will now be inspected by @checky when made by you: ${ mentions.join(', ') }.\nIf for any reason you want to make @checky start ignoring them again, reply to any of my posts with "!ignore username1 username2 ...".`, author, permlink, `Removed some ignored mentions for @${ author }`);
+            } else sendMessage('You didn\'t specify any username to unignore. Please try again by using the format "!unignore username1 username2 ...".', author, permlink, 'No username specified');
+            break;
+        default:
+            sendMessage('This command doesn\'t exist.', author, permlink, 'Unknown command');
     }
+}
+
+function sendMessage(message, author, permlink, title) {
     const metadata = {
         app: 'checky/0.0.1',
         format: 'markdown',
@@ -94,7 +152,7 @@ function sendMessage(wrongMentions, author, permlink, title) {
         ],
         users: [ author ]
     } 
-    steemRequest.broadcast.comment(posting_key, author, permlink, 'checky', 're-' + permlink + Math.random().toString(36).slice(2), 'Possible wrong mentions found on "' + title + '"', message, JSON.stringify(metadata), function(err, result) {
-        console.log(err, result);
+    steemRequest.broadcast.comment(posting_key, author, permlink, 'checky', 're-' + permlink, title, message, JSON.stringify(metadata), function(err) {
+        console.log(err);
     });
 }
