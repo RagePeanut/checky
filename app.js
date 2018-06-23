59781aad9b14ac025de48e704848959dc53f4afb
RagePeanut <dorian.gruselin@hotmail.com>
2018-06-23 22:12:32 +0200

    Made the blacklist into a user mode

--- app.js
+++ app.js
@@ -6,26 +6,15 @@ const { posting_key, request_nodes, stream_nodes } = require('./config');
 
 steemRequest.api.setOptions({ url: request_nodes[0] });
 
-let blacklist = [];
-let ignoredMentions = {};
 let users = {};
-if(fs.existsSync('data/')) {
-    if(fs.existsSync('data/blacklist.json')) blacklist = JSON.parse(fs.readFileSync('data/blacklist.json', 'utf8'));
-    if(fs.existsSync('data/ignored_mentions.json')) ignoredMentions = JSON.parse(fs.readFileSync('data/ignored_mentions.json', 'utf8'));
-    if(fs.existsSync('data/users.json')) users = JSON.parse(fs.readFileSync('data/users.json', 'utf8'));
-} else fs.mkdirSync('data');
+if(fs.existsSync('data') && fs.existsSync('data/users.json')) users = require('data/users');
+else fs.mkdirSync('data');
 
-setInterval(() => {
-    fs.writeFile('data/blacklist.json', JSON.stringify(blacklist), err => {
-        if(err) console.log(err);
-    });
-    fs.writeFile('data/ignored_mentions.json', JSON.stringify(ignoredMentions), err => {
-        if(err) console.log(err);
-    });
+setInterval(() => 
     fs.writeFile('data/users.json', JSON.stringify(users), err => {
         if(err) console.log(err);
-    });
-}, 60 * 1000);
+    })
+, 5 * 1000);
 
 stream();
 
@@ -34,22 +23,26 @@ function stream() {
         steemStream.api.setOptions({ url: stream_nodes[0] })
         steemStream.api.streamOperations((error, operation) => {
             if(error) return reject(error);
+
+            const author = operation[1].author;
+            const parent_author = operation[1].parent_author;
+            const body = operation[1].body;
+            const permlink = operation[1].permlink;
+
             if(operation && operation[0] === 'comment') {
-                if(operation[1].parent_author === 'checky') {
-                    const command = /[!/]([A-Za-z]+)(?:\s+(.+))?/.exec(operation[1].body);
-                    if(command[1]) processCommand(command, operation[1].author, operation[1].permlink);
-                } else if(operation[1].parent_author === '' || users[operation[1].author] && users[operation[1].author].mode === 'advanced') {
-                    if(!blacklist.includes(operation[1].author)) {
-                        try {
-                            const metadata = JSON.parse(operation[1].json_metadata);
-                            // Removing the punctuation at the end of some mentions, lower casing mentions, removing duplicates and already encountered existing users
-                            let mentions = _.uniq(metadata.users.map(user => user.replace(/[?¿!¡.,;:-]+$/, '').toLowerCase())).filter(user => !users[user]);
-                            // Removing ignored mentions
-                            if(ignoredMentions[operation[1].author]) mentions = mentions.filter(mention => !ignoredMentions[operation[1].author].includes(mention));
-                            if(!users[operation[1].author]) users[operation[1].author] = { mode: 'regular' };
-                            if(mentions.length > 0) processCreatedPost(mentions, operation[1].body, operation[1].author, operation[1].permlink);
-                        } catch(err) {}
-                    }
+                if(parent_author === 'checky') {
+                    const command = /[!/]([A-Za-z]+)(?:\s+(.+))?/.exec(body);
+                    if(command[1]) processCommand(command, author, permlink);
+                } else if(parent_author === '' && users[author].mode !== 'off' || users[author] && users[author].mode === 'advanced') {
+                    try {
+                        const metadata = JSON.parse(operation[1].json_metadata);
+                        // Removing the punctuation at the end of some mentions, lower casing mentions, removing duplicates and already encountered existing users
+                        let mentions = _.uniq(metadata.users.map(user => user.replace(/[?¿!¡.,;:-]+$/, '').toLowerCase())).filter(user => !users[user]);
+                        // Removing ignored mentions
+                        if(users[author]) mentions = mentions.filter(mention => !users[author].ignored.includes(mention));
+                        else users[author] = { mode: 'regular', ignored: [] };
+                        if(mentions.length > 0) processCreatedPost(mentions, body, author, permlink);
+                    } catch(err) {}
                 }
             }
         });
@@ -88,7 +81,7 @@ function processMentions(mentions, body, author, permlink, title) {
                     const regex = new RegExp('(?:^|[\\s\\S]{0,299}[^\\w/-])@' + _.escapeRegExp(mentions[i]) + '(?:[^\\w/-][\\s\\S]{0,299}|$)', 'gi');
                     const match = body.match(regex);
                     if(match && !/(insta|tele)gram|tw(itter|eet)|medium|brunch|텔레그램/i.test(match)) wrongMentions.push('@' + mentions[i]);
-                } else users[mentions[i]] = { mode: 'regular' };
+                } else users[mentions[i]] = { mode: 'regular', ignored: [] };
             }
             if(wrongMentions.length > 0) {
                 let message = `Hi @${ author },`;
@@ -116,31 +109,17 @@ function processMentions(mentions, body, author, permlink, title) {
 
 function processCommand(command, author, permlink) {
     switch(command[1]) {
-        case 'start':
-        case 'join':
-            if(blacklist.includes(author)) {
-                _.pull(blacklist, author);
-                sendMessage('Your mentions will from now on be inspected by @checky. If for any reason you want to stop this, reply to any of my posts with "!stop" or "!leave".', author, permlink, `Removed @${ author } from the blacklist`);
-            } else sendMessage('Your mentions were already being inspected by @checky.', author, permlink, 'The blacklist hasn\'t been changed (username not in blacklist)')
-            break;
-        case 'stop':
-        case 'leave':
-            blacklist = _.union(blacklist, [author]);
-            sendMessage('You\'ve been added to @checky\'s blacklist. Your mentions will from now on stop being inspected by @checky. If for any reason you want @checky to start checking your mentions again, reply to any of my posts with "!start" or "!join".', author, permlink, `Added @${ author } to the blacklist`);
-            break;
         case 'ignore':
             if(command[2]) {
                 const mentions = command[2].split(/[\s,]+/).filter(mention => mention !== '').map(mention => mention.replace('@', ''));
-                ignoredMentions[author] = _.union(ignoredMentions[author], mentions);
+                users[author].ignored = _.union(users[author].ignored, mentions);
                 sendMessage(`The following mentions will now be ignored when made by you: ${ mentions.join(', ') }.\nIf for any reason you want to make @checky stop ignoring them, reply to any of my posts with "!unignore username1 username2 ...".`, author, permlink, `Added some ignored mentions for @${ author }`);
             } else sendMessage('You didn\'t specify any username to ignore. Please try again by using the format "!ignore username1 username2 ...".', author, permlink, 'No username specified');
             break;
         case 'unignore':
             if(command[2]) {
                 const mentions = command[2].split(/[\s,]+/).filter(mention => mention !== '').map(mention => mention.replace('@', ''));
-                mentions.forEach(mention => {
-                    if(ignoredMentions[author]) _.pull(ignoredMentions[author], mention);
-                });
+                mentions.forEach(mention => _.pull(users[author].ignored, mention));
                 sendMessage(`The following mentions will now be inspected by @checky when made by you: ${ mentions.join(', ') }.\nIf for any reason you want to make @checky start ignoring them again, reply to any of my posts with "!ignore username1 username2 ...".`, author, permlink, `Removed some ignored mentions for @${ author }`);
             } else sendMessage('You didn\'t specify any username to unignore. Please try again by using the format "!unignore username1 username2 ...".', author, permlink, 'No username specified');
             break;
@@ -149,19 +128,24 @@ function processCommand(command, author, permlink) {
             if(command[2]) {
                 const mode = _.trim(command[2]);
                 switch(mode) {
+                    case 'on':
                     case 'regular':
                     case 'normal':
                         users[author].mode = 'regular';
                         sendMessage('Your account mode has been set to regular. You will now only get your mentions checked for posts you make.');
                         break;
                     case 'advanced':
+                    case 'plus':
                         users[author].mode = 'advanced';
                         sendMessage('Your account mode has been set to advanced. You will now get your mentions checked for posts and comments.');
                         break;
+                    case 'off':
+                        users[author].mode = 'off';
+                        sendMessage('Your account mode has been set to off. You will now get no mentions checked whatsoever.');
                     default:
-                        sendMessage(`The ${ mode } mode doesn't exist. Your account is currently set to ${ users[account].mode }, to switch it to ${ users[account].mode === 'regular' ? 'advanced' : 'regular' } please write "!mode ${ users[account].mode === 'regular' ? 'advanced' : 'regular' }".`, author, permlink, 'Wrong mode specified');
+                        sendMessage(`The ${ mode } mode doesn't exist. Your account is currently set to ${ users[account].mode }. To switch it to regular, advanced or off, please write "!mode regular/advanced/off".`, author, permlink, 'Wrong mode specified');
                 }
-            } else sendMessage('You didn\'t spectify any mode to switch to. Please try again by using "!mode regular" or "!mode advanced".', author, permlink, 'No mode specified');
+            } else sendMessage('You didn\'t spectify any mode to switch to. Please try again by using "!mode regular", "!mode advanced" or "!mode off".', author, permlink, 'No mode specified');
         default:
             sendMessage('This command doesn\'t exist.', author, permlink, 'Unknown command');
     }
