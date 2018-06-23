a4e66d7b8508d7165857e77d32142faee01c06ff
RagePeanut <dorian.gruselin@hotmail.com>
2018-06-23 19:20:18 +0200

    Added an advanced mode and reduced API usage

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ steemRequest.api.setOptions({ url: request_nodes[0] });
 
 let blacklist = [];
 let ignoredMentions = {};
+let users = {};
 if(fs.existsSync('data/')) {
     if(fs.existsSync('data/blacklist.json')) blacklist = JSON.parse(fs.readFileSync('data/blacklist.json', 'utf8'));
     if(fs.existsSync('data/ignored_mentions.json')) ignoredMentions = JSON.parse(fs.readFileSync('data/ignored_mentions.json', 'utf8'));
+    if(fs.existsSync('data/users.json')) users = JSON.parse(fs.readFileSync('data/users.json', 'utf8'));
 } else fs.mkdirSync('data');
+
 setInterval(() => {
     fs.writeFile('data/blacklist.json', JSON.stringify(blacklist), err => {
         if(err) console.log(err);
@@ -19,7 +22,10 @@ setInterval(() => {
     fs.writeFile('data/ignored_mentions.json', JSON.stringify(ignoredMentions), err => {
         if(err) console.log(err);
     });
-}, 30 * 1000);
+    fs.writeFile('data/users.json', JSON.stringify(users), err => {
+        if(err) console.log(err);
+    });
+}, 60 * 1000);
 
 stream();
 
@@ -29,20 +35,21 @@ function stream() {
         steemStream.api.streamOperations((error, operation) => {
             if(error) return reject(error);
             if(operation && operation[0] === 'comment') {
-                if(operation[1].parent_author === '%') {
+                if(operation[1].parent_author === 'checky') {
+                    const command = /[!/]([A-Za-z]+)(?:\s+(.+))?/.exec(operation[1].body);
+                    if(command[1]) processCommand(command, operation[1].author, operation[1].permlink);
+                } else if(operation[1].parent_author === '' || users[operation[1].author] && users[operation[1].author].mode === 'advanced') {
                     if(!blacklist.includes(operation[1].author)) {
                         try {
                             const metadata = JSON.parse(operation[1].json_metadata);
-                            // Removing the punctuation at the end of some mentions, lower casing mentions and removing duplicates
-                            let mentions = _.uniq(metadata.users.map(user => user.replace(/[?¿!¡.,;:-]+$/, '').toLowerCase()));
+                            // Removing the punctuation at the end of some mentions, lower casing mentions, removing duplicates and already encountered existing users
+                            let mentions = _.uniq(metadata.users.map(user => user.replace(/[?¿!¡.,;:-]+$/, '').toLowerCase())).filter(user => !users[user]);
                             // Removing ignored mentions
                             if(ignoredMentions[operation[1].author]) mentions = mentions.filter(mention => !ignoredMentions[operation[1].author].includes(mention));
+                            if(!users[operation[1].author]) users[operation[1].author] = { mode: 'regular' };
                             if(mentions.length > 0) processCreatedPost(mentions, operation[1].body, operation[1].author, operation[1].permlink);
                         } catch(err) {}
                     }
-                } else if(operation[1].parent_author === 'checky') {
-                    const command = /!([A-Za-z]+)(?:\s+(.+))?/.exec(operation[1].body);
-                    if(command[1]) processCommand(command, operation[1].author, operation[1].permlink);
                 }
             }
         });
@@ -74,14 +81,14 @@ function processMentions(mentions, body, author, permlink, title) {
     new Promise((resolve, reject) => {
         steemRequest.api.lookupAccountNames(mentions, (err, res) => {
             if(err) return reject(err);
-            const wrongMentions = [];
+            let wrongMentions = [];
             // Add each username that got a null result from the API (meaning the user doesn't exist) to the wrongMentions array
             for(let i = 0; i < mentions.length; i++) {
                 if(res[i] === null) {
                     const regex = new RegExp('(?:^|[\\s\\S]{0,299}[^\\w/-])@' + _.escapeRegExp(mentions[i]) + '(?:[^\\w/-][\\s\\S]{0,299}|$)', 'gi');
                     const match = body.match(regex);
                     if(match && !/(insta|tele)gram|tw(itter|eet)|medium|brunch|텔레그램/i.test(match)) wrongMentions.push('@' + mentions[i]);
-                }
+                } else users[mentions[i]] = { mode: 'regular' };
             }
             if(wrongMentions.length > 0) {
                 let message = `Hi @${ author },`;
@@ -137,6 +144,24 @@ function processCommand(command, author, permlink) {
                 sendMessage(`The following mentions will now be inspected by @checky when made by you: ${ mentions.join(', ') }.\nIf for any reason you want to make @checky start ignoring them again, reply to any of my posts with "!ignore username1 username2 ...".`, author, permlink, `Removed some ignored mentions for @${ author }`);
             } else sendMessage('You didn\'t specify any username to unignore. Please try again by using the format "!unignore username1 username2 ...".', author, permlink, 'No username specified');
             break;
+        case 'mode':
+        case 'switch':
+            if(command[2]) {
+                const mode = _.trim(command[2]);
+                switch(mode) {
+                    case 'regular':
+                    case 'normal':
+                        users[author].mode = 'regular';
+                        sendMessage('Your account mode has been set to regular. You will now only get your mentions checked for posts you make.');
+                        break;
+                    case 'advanced':
+                        users[author].mode = 'advanced';
+                        sendMessage('Your account mode has been set to advanced. You will now get your mentions checked for posts and comments.');
+                        break;
+                    default:
+                        sendMessage(`The ${ mode } mode doesn't exist. Your account is currently set to ${ users[account].mode }, to switch it to ${ users[account].mode === 'regular' ? 'advanced' : 'regular' } please write "!mode ${ users[account].mode === 'regular' ? 'advanced' : 'regular' }".`, author, permlink, 'Wrong mode specified');
+                }
+            } else sendMessage('You didn\'t spectify any mode to switch to. Please try again by using "!mode regular" or "!mode advanced".', author, permlink, 'No mode specified');
         default:
             sendMessage('This command doesn\'t exist.', author, permlink, 'Unknown command');
     }
@@ -148,10 +173,11 @@ function sendMessage(message, author, permlink, title) {
         format: 'markdown',
         tags: [ 
             'mentions',
-            'bot'
+            'bot',
+            'checky'
         ],
         users: [ author ]
-    } 
+    }
     steemRequest.broadcast.comment(posting_key, author, permlink, 'checky', 're-' + permlink, title, message, JSON.stringify(metadata), function(err) {
         console.log(err);
     });
