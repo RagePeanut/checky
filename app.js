543fea903796fe7fd7130f5931c1ee8698d1ddb3
RagePeanut <dorian.gruselin@hotmail.com>
2018-07-11 20:54:12 +0200

    Completed the operations list

--- app.js
+++ app.js
@@ -56,17 +56,99 @@ function stream() {
                         }
                         break;
                     case 'vote':
-                        addUsers(operation[1].voter, operation[1].author);
+                        const voter = operation[1].voter;
+                        addUsers(voter, operation[1].author);
                         // Setting the user mode to off if he has flagged the bot's comment
                         if(operation[1].author === 'checky' && operation[1].weight < 0) users[voter].mode = 'off';
                         break;
+                    case 'delegate_vesting_shares':
+                        addUsers(operation[1].delegator, operation[1].delegatee);
+                        break;
+                    case 'escrow_release':
+                        addUsers(operation[1].from, operation[1].to, operation[1].agent, operation[1].who, operation[1].receiver);
+                        break;
+                    case 'escrow_approve':
+                    case 'escrow_dispute':
+                        addUsers(operation[1].from, operation[1].to, operation[1].agent, operation[1].who);
+                        break;
+                    case 'escrow_transfer':
+                        addUsers(operation[1].from, operation[1].to, operation[1].agent);
+                        break;
+                    case 'fill_transfer_from_savings':
                     case 'transfer':
                     case 'transfer_to_vesting':
+                    case 'transfer_to_savings':
+                    case 'transfer_from_savings':
                         addUsers(operation[1].from, operation[1].to);
                         break;
+                    case 'fill_vesting_withdraw':
+                    case 'set_withdraw_vesting_route':
+                        addUsers(operation[1].from_account, operation[1].to_account);
+                        break;
+                    case 'fill_order':
+                        addUsers(operation[1].current_owner, operation[1].open_owner);
+                        break;
+                    case 'request_account_recovery':
+                        addUsers(operation[1].recovery_account, operation[1].account_to_recover);
+                        break;
+                    case 'change_recovery_account':
+                        addUsers(operation[1].account_to_recover, operation[1].new_recovery_account);
+                        break;
+                    case 'account_create':
+                    case 'account_create_with_delegation':
+                        addUsers(operation[1].creator, operation[1].new_account_name);
+                        break;
+                    case 'account_witness_vote':
+                        addUsers(operation[1].account, operation[1].witness);
+                        break;
+                    case 'account_witness_proxy':
+                        addUsers(operation[1].account, operation[1].proxy);
+                        break;
+                    case 'account_update':
                     case 'claim_reward_balance':
+                    case 'decline_voting_rights':
+                    case 'return_vesting_delegation':
+                    case 'withdraw_vesting':
                         addUsers(operation[1].account);
                         break;
+                    case 'convert': 
+                    case 'fill_convert_request':
+                    case 'interest':
+                    case 'limit_order_create':
+                    case 'limit_order_create2':
+                    case 'limit_order_cancel':
+                    case 'liquidity_reward':
+                    case 'shutdown_witness':
+                    case 'witness_update':
+                        addUsers(operation[1].owner);
+                        break;
+                    case 'author_reward':
+                    case 'comment_options':
+                    case 'comment_payout_update':
+                    case 'comment_reward':
+                    case 'delete_comment':
+                        addUsers(operation[1].author);
+                        break;
+                    case 'curation_reward':
+                        addUsers(operation[1].curator, operation[1].comment_author);
+                        break;
+                    case 'feed_publish':
+                        addUsers(operation[1].publisher);
+                        break;
+                    case 'recover_account':
+                        addUsers(operation[1].account_to_recover);
+                        break;
+                    case 'cancel_transfer_from_savings':
+                        addUsers(operation[1].from);
+                        break;
+                    case 'comment_benefactor_reward':
+                        addUsers(operation[1].benefactor, operation[1].author);
+                        break;
+                    case 'producer_reward':
+                        addUsers(operation[1].producer);
+                        break;
+                    case 'prove_authority':
+                        addUsers(operation[1].challenged);
                 }
             }
         });
@@ -152,14 +234,14 @@ function processCommand(command, author, permlink) {
     switch(command[1]) {
         case 'ignore':
             if(command[2]) {
-                const mentions = command[2].split(/[\s,]+/).filter(mention => mention !== '').map(mention => mention.replace('@', ''));
+                const mentions = command[2].split(/[\s,]+/).filter(mention => mention !== '').map(mention => mention.replace('@', '').toLowerCase());
                 users[author].ignored = _.union(users[author].ignored, mentions);
                 sendMessage(`The following mentions will now be ignored when made by you: ${ mentions.join(', ') }.\nIf for any reason you want to make @checky stop ignoring them, reply to any of my posts with \`!unignore username1 username2 ...\`.`, author, permlink, `Added some ignored mentions for @${ author }`);
             } else sendMessage('You didn\'t specify any username to ignore. Please try again by using the format `!ignore username1 username2`.', author, permlink, 'No username specified');
             break;
         case 'unignore':
             if(command[2]) {
-                const mentions = command[2].split(/[\s,]+/).filter(mention => mention !== '').map(mention => mention.replace('@', ''));
+                const mentions = command[2].split(/[\s,]+/).filter(mention => mention !== '').map(mention => mention.replace('@', '').toLowerCase());
                 mentions.forEach(mention => _.pull(users[author].ignored, mention));
                 sendMessage(`The following mentions will now be checked by @checky when made by you: ${ mentions.join(', ') }.\nIf for any reason you want to make @checky start ignoring them again, reply to any of my posts with \`!ignore username1 username2 ...\`.`, author, permlink, `Removed some ignored mentions for @${ author }`);
             } else sendMessage('You didn\'t specify any username to unignore. Please try again by using the format `!unignore username1 username2`.', author, permlink, 'No username specified');
@@ -191,9 +273,9 @@ function processCommand(command, author, permlink) {
             } else sendMessage(`You didn't spectify any mode to switch to. Please try again by using \`!${ command[1] } regular\`, \`!${ command[1] } advanced\` or \`!${ command[1] } off\`.`, author, permlink, 'No mode specified');
             break;
         case 'state':
-            let ignored = '';
-            if(users[author].ignored.length > 0) ignored = ' The following usernames are being ignored by @checky: ' + users[author].ignored.join(', ') + '.';
-            sendMessage(`Your account is currently set to ${ users[author].mode }.${ ignored }`, author, permlink, 'Account state');
+            let ignored = 'No mentions are being ignored by @checky';
+            if(users[author].ignored.length > 0) ignored = 'The following mentions are being ignored by @checky: ' + users[author].ignored.join(', ');
+            sendMessage(`Your account is currently set to ${ users[author].mode }. ${ ignored }.`, author, permlink, 'Account state');
             break;
         case 'help':
             const message = '#### Here are all the available commands:\n* **!help** **-** gives a list of commands and their explanations.\n* **!ignore** *username1* *username2* **-** tells  the bot to ignore some usernames mentionned in your posts (useful to avoid the bot mistaking other social network accounts for Steem accounts).\n* **!mode** *[regular-advanced-off]* **-** sets the mentions checking to regular (only posts), advanced (posts and comments) or off (no checking). Alternatively, you can write *normal* or *on* instead of *regular*. You can also write *plus* instead of *advanced*.\n* **!state** **-** gives the state of your account (*regular*, *advanced* or *off*).\n* **!switch** *[regular-advanced-off]* **-** same as **!mode**.\n* **!unignore** *username1* *username2* **-** tells the bot to unignore some usernames mentionned in your posts.\n\n###### Any idea on how to improve this bot ? Please contact @ragepeanut on any of his posts or send him a direct message on discord (RagePeanut#8078).';
@@ -223,7 +305,7 @@ function sendMessage(message, author, permlink, title) {
     }
     const footer = '\n\n###### If you found this comment useful, consider upvoting it to help keep this bot running. You can see a list of all available commands by replying with `!help`.';
     steemRequest.broadcast.comment(postingKey, author, permlink, 'checky', 're-' + author + '-' + permlink, title, message + footer, JSON.stringify(metadata), function(err) {
-        console.log(err);
+        if(err) console.error(err);
     });
 }
 
