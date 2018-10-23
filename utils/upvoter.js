206e946a7fb3acc56bf8077c79d74bccc11f5a9e
RagePeanut <dorian.gruselin@hotmail.com>
2018-10-24 00:51:14 +0200

    Added the structure for auto-saving the upvoter's state

--- utils/upvoter.js
+++ utils/upvoter.js
@@ -1,35 +1,55 @@
 let steemer = require('./steemer');
 const { test_environment } = require('../config');
 
-let candidates = [];
+let save = {
+    candidates: [],
+    not_checked: []
+};
 
 /**
  * Initializes the upvoter
  * @param {steemer} _steemer The instance of steemer used by the bot
  */
 function init(_steemer) {
+    // Updating `save` with the content of ./data/save.json if the file exists 
+    if(fs.existsSync('data')) {
+        if(fs.existsSync('data/save.json')) save = require('../data/save');
+    } else fs.mkdirSync('data');
     steemer = _steemer;
     setInterval(upvoteRandomCandidate, test_environment ? 1.5 * 60 * 60 * 1000 : (24 / 9) * 60 * 60 * 1000); // 90 minutes in test environment, ~2.66 hours in production environment
 }
 
 /**
- * Adds a candidate to the upvote `candidates`
+ * Adds a candidate to the upvote candidates
  * @param {string} author The author of the post
  * @param {string} permlink The permlink of the post
  */
 function addCandidate(author, permlink) {
-    candidates.push({author, permlink});
+    save.candidates.push({author, permlink});
+    updateSaveFile();
 }
 
 /**
- * Upvotes a random candidate from the upvote `candidates`
+ * Updates the ./data/save.json file with the content of the save object (recursively called every `interval` seconds)
  */
-function upvoteRandomCandidate() {
-    if(candidates.length > 0) {
-        const candidate = candidates[Math.floor(Math.random() * candidates.length)];
-        candidates = [];
+function updateSaveFile() {
+    steno.writeFile('data/save.json', JSON.stringify(save), err => {
+        if(err && log_errors) console.error(err.message);
+    });
+}
+
+/**
+ * Upvotes a random candidate from the upvote candidates
+ */
+async function upvoteRandomCandidate() {
+    if(save.candidates.length > 0) {
+        const candidate = save.candidates[Math.floor(Math.random() * save.candidates.length)];
+        save.candidates = [];
         if(test_environment) console.log(candidate);
-        else steemer.broadcastUpvote(candidate.author, candidate.permlink);
+        else {
+            await steemer.broadcastUpvote(candidate.author, candidate.permlink);
+            updateSaveFile();
+        }
     }
 }
 
